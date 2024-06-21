e2cc2c8d895f9ff5a2295509d8524564078b0cd7
raziqpv333 <raziqpv333@gmail.com>
2024-06-21 09:39:26 +0530

    startingjavascript

---
+++ one1.js
@@ -0,0 +1,20 @@
+function fun1(){
+    let a=parseInt(document.getElementById("a1").value)
+    let b=parseInt(document.getElementById("a2").value)
+    alert(a+b)
+}
+function fun2(){
+    let a=parseInt(document.getElementById("a1").value)
+    let b=parseInt(document.getElementById("a2").value)
+    alert(a-b)
+}
+function fun3(){
+    let a=parseInt(document.getElementById("a1").value)
+    let b=parseInt(document.getElementById("a2").value)
+    alert(a*b)
+}
+function fun4(){
+    let a=parseInt(document.getElementById("a1").value)
+    let b=parseInt(document.getElementById("a2").value)
+    alert(a/b)
+}
