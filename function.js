7a660ff2836fe70c3ee6cea7c8e4dd69474dbad2
raziqpv333 <162957798+raziqpv333@users.noreply.github.com>
2024-06-29 08:59:39 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,29 @@
+// function fun1(){
+//     console.log("welcome")
+// }
+// fun1()
+
+// function fun2(name){
+//     console.log("welcome",name)
+// }
+// fun2("baasi")
+
+// let fun3=function(){
+//     console.log("welcome")
+// }
+// fun3()
+
+// let fun4=function(name){
+//     console.log("welcome",name)
+// }
+// fun4("sachuu")
+
+// let fun5=()=>{
+//     console.log("welcome")
+// }
+// fun5()
+
+let fun6=(name)=>{
+    console.log("welcome",name)
+}
+fun6("faru")
