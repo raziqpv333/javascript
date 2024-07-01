77f68286c1fb5f4b6a304a5e317534c7a50ffc6d
raziqpv333 <162957798+raziqpv333@users.noreply.github.com>
2024-07-01 09:25:29 +0530

    Add files via upload

---
+++ arrayiteration.js
@@ -0,0 +1,10 @@
+// const numbers=[10,20,30,40];
+// const numbers2=numbers1.map(value=>value*2)
+// const numbers1=numbers.filter(value=>value>10)
+// const numbers2=numbers.reduce((total,value)=>total+=value)
+const month1=["jan","feb","mar"]
+const month2=["apr","may","june"]
+const month3=["july","aug","sep"]
+const month4=["oct","nov","dec"]
+const year=[...month1,...month2,...month3,...month4]
+console.log(year)
