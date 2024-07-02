05a8e93761b8e3f56786de5f7bd10c11cc4b7efb
raziqpv333 <162957798+raziqpv333@users.noreply.github.com>
2024-07-02 09:24:22 +0530

    Add files via upload

---
+++ destructing.js
@@ -0,0 +1,14 @@
+// let  a=[1,2,3,4]
+
+// let [a1,b1]=a
+// console.log(a1);
+// console.log(b1);
+
+
+const person = {
+    firstName: "John",
+    lastName: "tharakkal",
+    age: 50
+  };
+  let {firstName, lastName} = person;
+  console.log(firstName,lastName);
